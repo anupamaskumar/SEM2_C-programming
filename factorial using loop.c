2aeca0b755d4f49b02f98f32786c0c980f8faab4
anupamaskumar <85632175+anupamaskumar@users.noreply.github.com>
2021-07-18 22:50:43 +0530

    Add files via upload
    created a C program to find the factorial of a number using loop

---
+++ factorial using loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int num, count, fact = 1;
+	
+	printf("Enter a number to find factorial\n");
+	scanf("%d", &num);
+	
+	for(count = 1; count <= num; count++)
+	{
+		fact = fact * count;
+	}
+	
+	printf("Factorial of %d is %d\n", num, fact);
+	
+	return 0;
+}
