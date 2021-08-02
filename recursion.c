990d202bdc4d1300962a7bcd79a53cc9ef56c2e4
anupamaskumar <85632175+anupamaskumar@users.noreply.github.com>
2021-08-02 05:57:26 +0530

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int fact(int);
+
+int main()
+{
+	int num;
+	
+	printf("Enter a positive number to find it's factorial\n");
+	scanf("%d", &num);
+	
+	printf("\nFactorial of %d is %d.\n", num,fact(num));
+	
+	return 0;
+}
+
+int fact(int num)
+{
+	if(num)
+	    return(num*fact(num - 1));
+	else
+	    return 1;
+}
+
+	
+	
+	
+
